824e4960c48c889f2628fac111cfdaf4622f6f17
Andrei <pripandrei@gmail.com>
2023-06-14 15:06:06 +0300

    adjusted uiDocument to load and export json data

--- DocumentImageGallery/ImageGalleryDocument.swift
+++ DocumentImageGallery/ImageGalleryDocument.swift
@@ -9,16 +9,19 @@ import UIKit
 
 class ImageGalleryDocument: UIDocument {
     
-    
+    var imageGallery: ImageGallery?
     
     override func contents(forType typeName: String) throws -> Any {
         // Encode your document with an instance of NSData or NSFileWrapper
-        return Data()
+        return imageGallery?.json ?? Data()
     }
     
     override func load(fromContents contents: Any, ofType typeName: String?) throws {
         // Load your document from contents
-      
+        guard let json = contents as? Data else {
+            return
+        }
+        imageGallery = ImageGallery(json: json)
     }
 }
 
